6c0c97d26eea8738402e9decc4bdd427fb05acf2
Bastien Selzner <bastien.selzner@gmail.com>
2022-07-15 13:34:27 +0200

    Ajout Interface ISortSymptomDataFromArrayList.java

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISortSymptomDataFromArrayList.java
@@ -0,0 +1,11 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+import java.util.TreeMap;
+
+public interface ISortSymptomDataFromArrayList {
+	
+	public TreeMap<String, Integer> sortingSymptomsData(List<String> allSymptomsList);
+	
+
+}
